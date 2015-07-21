58e973fd750103b8583b78f3952b33ea7e79fe52
patrickcousins <patrick@thehackerati.com>
2015-07-21 15:25:47 -0400

    Load db schemea in main py script

--- app/flaskr/flaskr.py
+++ app/flaskr/flaskr.py
@@ -2,6 +2,7 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+from contextlib import closing
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
@@ -18,6 +19,12 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 if __name__ == '__main__':
     app.run()
