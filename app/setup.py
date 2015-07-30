d233c3f4b35adacda0e099b87e8e1e670c1846ee
ricky25j <ricky26j@yahoo.com>
2015-07-30 15:52:31 -0400

    added setup.py

---
+++ app/setup.py
@@ -0,0 +1,23 @@
+.core import setup
+
+setup(
+    # Application name:
+    name="flask-app-template",
+
+
+
+    # Packages
+    packages=["app"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "flask",
+    ],
+)
