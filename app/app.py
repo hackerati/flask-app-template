653a4d17730be619fe1650717290f34c3526d63a
ricky25j <ricky26j@yahoo.com>
2015-07-30 14:42:21 -0400

    created flask app

---
+++ app/app.py
@@ -0,0 +1,25 @@
+import os
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+
+# create our little application :)
+app = Flask(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+@app.route('/')
+def home():
+	return render_template('helloworld.html')
+
+if __name__ == '__main__':
+    app.run()
