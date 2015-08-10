115d13ef78aee5065c17968bbab010235399a1cd
ricky25j <ricky26j@yahoo.com>
2015-08-10 12:06:08 -0400

    printed hello world directly instead of rendering template

--- app/app.py
+++ app/app.py
@@ -20,7 +20,7 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 """
 @app.route('/')
 def home():
-	return render_template('helloworld.html')
+	return "Hello World"
 
 if __name__ == '__main__':
     app.run()
