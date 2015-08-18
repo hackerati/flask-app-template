87acac7fafccf45a8d194961f147eb7875569f13
ricky25j <ricky26j@yahoo.com>
2015-08-18 16:34:34 -0400

    delete appenv to simplify

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
-app.config['APP_ROOT'] = '/flask-app-template/app'
+#app.config['APP_ROOT'] = '/flask-app-template'
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
