cf10581bc941424a7c89d87e4ed5682423339767
ricky25j <ricky26j@yahoo.com>
2015-09-01 14:35:24 -0400

    app renders template rather than printing directly

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 application.config['APPLICATION_ROOT'] = '/flask-app-template'
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return render_template("HelloWorld.html")
